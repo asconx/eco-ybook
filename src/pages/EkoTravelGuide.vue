4113a688e7cbd14b24fd5d5935f35bbe3a27a049
DESKTOP-GKVSUDO\User <ropekpop@gmail.com>
2025-01-16 17:53:17 +0100

    MB

---
+++ src/pages/EkoTravelGuide.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout>
+    <HeaderComponent />
+    <q-page-container>
+     <h1>Hello world</h1>
+    </q-page-container>
+    <FooterComponent />
+  </q-layout>
+</template>
+
+<script lang="ts" setup>
+import FooterComponent from "components/FooterComponent.vue";
+import HeaderComponent from "components/HeaderComponent.vue";</script>
+<style lang="sass" scoped>
+.text-subtitle1
+  font-size: 1.1rem
+</style>
