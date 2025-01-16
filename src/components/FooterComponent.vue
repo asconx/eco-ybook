3c02d51a66b702c96adbde90ff0f7f0904259009
KOMP-4\Galikowski3TP <stefankrzysiu@gmail.com>
2025-01-16 13:21:40 +0100

    AG

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -16,7 +16,7 @@
         <h3 class="text-h6 font-bold">Prawnie i poważnie</h3>
         <p><a href="#">Zasady i regulaminy</a></p>
         <p><a href="#">Polityka prywatności</a></p>
-        <p><a href="#">Polityka plików cookies</a></p>
+        <p><a href="cookies">Polityka plików cookies</a></p>
       </div>
 
       <div class="col-12 col-md-4">
